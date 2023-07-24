03e9785885309229d148016cdfdad430a1ce958d
zharvis <fatih.mehmed.cin@gmail.com>
2023-07-24 19:36:26 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+// initialize modules
+const { series, watch } = require("gulp");
+const browsersync = require("browser-sync").create();
+
+// Browsersync
+function browserSyncServe(cb) {
+  browsersync.init({
+    server: {
+      baseDir: ".",
+    },
+    notify: {
+      styles: {
+        top: "auto",
+        bottom: "0",
+      },
+    },
+    open: false,
+  });
+  cb();
+}
+
+function browserSyncReload(cb) {
+  browsersync.reload();
+  cb();
+}
+
+// Watch Task
+function watchTask() {
+  watch(["index.html", "style.css"], browserSyncReload);
+}
+
+// Default Gulp Task
+exports.default = series(browserSyncServe, watchTask);
